0bb51e54df40fc409e3e9b1d72c48a45dcb305e5
kylemiller13 <millerkyle85@gmail.com>
2022-12-16 22:21:55 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Resume from './Resume';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      < Resume />
+    </React.Fragment>
+  );
+}
+
+export default App;

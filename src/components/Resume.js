e44e56b024609f1428a689c6623a51d96c93b22d
kylemiller13 <millerkyle85@gmail.com>
2022-12-17 11:20:53 -0800

    add skils, add link the project

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Paper, Typography, Button, Divider, List, ListItem, ListItemSecondaryAction, Collapse } from '@material-ui/core';
+import {Paper, Typography, Button, Divider, List, ListItem, ListItemSecondaryAction, Collapse , Link} from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import PhoneIcon from "@material-ui/icons/Phone";
 import GitHubIcon from "@material-ui/icons/GitHub";
@@ -71,13 +71,14 @@ function Resume() {
             </Button>
           </Typography>
           <Typography  align="left" variant="h5"><strong>Skills</strong></Typography>
-          <Divider variant="fullWidth" style={{ backgroundColor: '#000' }} /> 
-
-            <p>Some content below the divider.</p>
+          <Divider variant="fullWidth" style={{ backgroundColor: '#000' }} />
+          <List type="ordered"  style={{ paddingTop: 0 }}>
+            <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• HTML • CSS/Bootstrap • JavaScript • Ruby on Rails • React • Test Driven Development • Material UI • Git • MySQL/PSQL </ListItem>
+          </List>
             <Typography  align="left" variant="h5"><strong>Projects</strong></Typography>
             <Divider variant="fullWidth" style={{ backgroundColor: '#000', }} />
             <List type="ordered"  style={{ paddingTop: 0 }}>
-              <ListItem style={{ paddingBottom: 0 }}><strong>Weather Site</strong></ListItem>
+              <ListItem style={{ paddingBottom: 0 }}><Link href="https://kylesweathersite.netlify.app/"><strong>Weather Site</strong></Link></ListItem>
               <ListItem style={{ paddingTop: 0, paddingBottom: 0, fontStyle: 'italic' }}>ReactJS</ListItem>
               <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• A weather website where a user can choose a selected city or enter the desired place.</ListItem>
               <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• Uses OpenWeatherMap API to display data for today’s forecast and a six-day future forecast.</ListItem>

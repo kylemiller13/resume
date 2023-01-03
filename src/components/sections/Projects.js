57793ad87db7aa926536b757c8ca91e39d43b75b
kylemiller13 <millerkyle85@gmail.com>
2023-01-03 12:59:31 -0800

    add project

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -10,6 +10,13 @@ function Projects() {
           <ListItem style={{ paddingTop: 0, paddingBottom: 0, fontStyle: 'italic' }}>ReactJS</ListItem>
           <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• A website that allows a user to select a specific city or enter a desired location to get weather information.</ListItem>
           <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• The OpenWeatherMap API is used to display data for the current forecast and a forecast for the next seven days.</ListItem>
+          <ListItem style={{ paddingBottom: 0 }}><Link href="https://github.com/pangtodd/bike_gallery"><strong>Bike Gallery</strong></Link></ListItem>
+          <ListItem style={{ paddingTop: 0, paddingBottom: 0, fontStyle: 'italic' }}>Ruby on Rails</ListItem>
+          <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• Pair Programmed with a team of 4 Epicodus students to create a Bike Gallery Site.</ListItem>
+          <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• Installed with full CRUD functionality and Authorization via Devise.</ListItem>
+          <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• Uses Active Storage and AWS to allow admin users to upload a photo.</ListItem>
+
+
         </List>
     </div>
   );

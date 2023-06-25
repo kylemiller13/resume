c84bb38175f5c785bd97f6cd57da8be02ff95b76
kylemiller13 <millerkyle85@gmail.com>
2023-06-24 17:29:23 -0700

    add opine website

--- src/components/sections/Experience.js
+++ src/components/sections/Experience.js
@@ -13,7 +13,7 @@ function Experience() {
             </ListItemSecondaryAction>
           </ListItem>
           <ListItem style={{ paddingTop: 0, paddingBottom: 0, fontStyle: 'italic' }}>Software Engineer Intern</ListItem>
-          <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• Conceptualized and redesigned the Opine web application using responsive UI elements(Material UI) and React framework</ListItem>
+          <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• Conceptualized and redesigned&nbsp;<a href="https://opineschool.com/" target="_blank" rel="noopener noreferrer"> Opine web application</a>&nbsp;frontend using responsive UI elements(Material UI) and React framework</ListItem>
           <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• Engaged in daily scrum meetings to discuss progress and any issues that may arise</ListItem>
           <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• Worked to continuously improve the application by troubleshooting, debugging, and adding new features</ListItem>
           <ListItem style={{ paddingTop: 0, marginLeft: '1em', paddingBottom: 0 }}>• Collaborated with team members to allow us to design and plan the workflow for our development tasks</ListItem>
